dd903bb3fc1dcf41c3abfb5eef2e21731f3df757
Goldie <goldiejackson25@yahoo.com>
2021-09-04 17:32:47 -0500

    commit tree 721c481d121978b8561b5e21df1ab7ec9a58b6f9

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Code your solution here
+
+//findMatching()
+/* 1) returns all drivers that match the passed in name
+2) returns matching drivers if case does not match but letters do
+3) returns an empty array if there is no match */
+/* function findMatching(driverNames, string){
+    driverNames.find(function (){
+        return x === string;
+    });
+} */
+
+function findMatching(arrayOfDrivers, stringToMatch)
+{   
+    const newArrayOfDrivers = arrayOfDrivers.filter(x => x.toUpperCase() === stringToMatch.toUpperCase());
+
+    return newArrayOfDrivers;
+}
+
+//const names = ["sally", "sammy", "bobby"];
+//debugger;
+//const letters = "sa"
+
+function fuzzyMatch(arrayOfDrivers, stringOfLetters){
+    const newArray = arrayOfDrivers.filter(x => x[0] + x[1] === stringOfLetters);
+    return newArray;
+    }
+
+function matchName(arrayOfObjects, stringToMatch){
+    //look through array for name object properties
+    const newArray = arrayOfObjects.filter(x => x["name"] === stringToMatch);
+    return newArray;
+    //return ===stringtomatch
+}
+
+
